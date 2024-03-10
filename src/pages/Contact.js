6305896b7c40f3841812f109f87123b169c9111e
Joshua Williams <joshuawilliamswebster@gmail.com>
2024-03-10 16:55:59 +0000

    working commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,23 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='absolute p-4 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'>
-      <p>Text</p>
+      <section class="">
+        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-700">Contact Us</h2>
+            <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 text-xl">Thank you for visiting! We hope that you have found what you needed on our website. If not, please feel free to contact us using any of the methods below!</p>
+            <div class="space-y-8">
+              <div>
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-700">Your email</label>
+              </div>
+              <div>
+                  <label for="subject" class="block mb-2 text-sm font-medium text-gray-700">Subject</label>
+              </div>
+              <div class="">
+                  <label for="message" class="block mb-2 text-sm font-medium text-gray-700">Your message</label>
+              </div>
+            </div>
+        </div>
+      </section>
     </div>
   )
 }
--- src/pages/components/AlternatingText.js
+++ src/pages/components/AlternatingText.js
@@ -5,34 +5,49 @@ import React from 'react';
 const AlternatingText = () => {
     const content = [
         {
-          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
+          title: "Refill.ie Initiative",
+          text: "Refill Ireland is an environmental project to make Irish towns and cities tap water refill friendly for everyone while on the go.",
           image: "https://via.placeholder.com/150"
         },
         {
-          text: "Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+          title: "Doolin Tourism",
+          text: "Situated on the Wild Atlantic Way, boasting some of the most breathtaking scenery in Ireland and with a colourful and welcoming village ready to host you during your stay.",
+          image: "https://via.placeholder.com/150"
+        },
+        {
+          title: "Ecotourism & our UNESCO Geopark",
+          text: "We are proud members of the Burren Ecotourism Network, committed to the promotion of responsible tourism that conserves the environment and improves the well-being of local people. We work together to promote ‘The Burren & Cliffs of Moher UNESCO Global Geopark’ as a leading sustainable visitor destination, celebrated for high standards in visitor experience, conservation & learning.",
+          image: "https://via.placeholder.com/150"
+        },
+        {
+          title: "Environmental Policy for Doolin Pantry",
+          text: "The staff and management of the Doolin Pantry are committed to our Environmental Policy and Plan.  This plan aims to reduce our carbon footprint by reducing our water and energy consumption and reducing our waste production and increase our level of recycling further, without impacting on the enjoyment of our customers.",
+          image: "https://via.placeholder.com/150"
+        },
+        {
+          title: "Burren Ecotourism Network",
+          text: "The Burren Ecotourism Network (the B.E.N.) is a membership organisation embedded in a sustainable tourism ethos.  It was formally established in 2011.",
           image: "https://via.placeholder.com/150"
         },
         // Add more objects as needed
       ];
+  
+  var titleAndPara = (title, text) => {
+    return <div className='flex flex-col'>
+      <b>{title}</b>
+      <p>{text}</p>
+    </div>
+  }
+  
   return (
     <div className="flex flex-col">
-      {content.map((item, index) => (
-        <div key={index} className={`${index %  2 ===  0 ? 'md:pr-4' : 'md:pl-4'} md:w-1/2`}>
-          <div className="flex flex-col md:flex-row">
-            {index %  2 ===  0 && (
-              <div className="md:w-1/2">
-                <p>{item.text}</p>
-              </div>
-            )}
-            <div className={`${index %  2 ===  0 ? 'md:w-1/2' : 'md:w-1/2'} md:flex md:justify-center`}>
-              <img src={item.image} alt="Sample" className="w-full md:w-auto" />
-            </div>
-            {index %  2 !==  0 && (
-              <div className="md:w-1/2">
-                <p>{item.text}</p>
-              </div>
-            )}
+      {content.map((item, i) => (
+        <div className='flex flex-row'>
+          {i % 2 === 0 && titleAndPara(item.title, item.text)}
+          <div>
+            <img src={item.image} alt="default pic" />
           </div>
+          {i % 2 !== 0 && titleAndPara(item.title, item.text)}
         </div>
       ))}
     </div>
